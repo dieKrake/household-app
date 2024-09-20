8cc49e945da4776d5fc3c34160dbec6776cb1be3
dieKrake <kkrazius@gmail.com>
2024-09-20 11:06:18 +0200

    outsourced TaskBoxComp

--- src/app/dashboard/activities/page.tsx
+++ src/app/dashboard/activities/page.tsx
@@ -1,43 +1,10 @@
+import TaskBox from "@/components/task-box";
 import { activityList } from "@/lib/dummy-data/activity-list";
 
 export default function Activities() {
   return (
     <div className="flex flex-wrap h-full w-full justify-around items-start text-lg">
-      {activityList.map((activity) => (
-        <div
-          key={activity.id}
-          className="w-64 h-60 sm:w-56 sm:h-56 md:w-48 md:h-48 mx-2 mb-5"
-        >
-          <div className="flex flex-col h-full justify-between p-2 gap-2 rounded-xl bg-gray-50 dark:bg-gray-900">
-            <div className="text-center">
-              <p className="mt-2 text-xl text-gray-950 dark:text-light">
-                {activity.activityName}
-              </p>
-            </div>
-
-            <div className="flex flex-wrap justify-center gap-2 p-2">
-              {Array.from({ length: activity.totalReps }, (_, index) => (
-                <div
-                  key={index}
-                  className={`w-6 h-6 rounded-full border dark:border-dark border-semiDark ${
-                    index < activity.progress
-                      ? "bg-semiLight dark:bg-semiLight"
-                      : "bg-gray-100 dark:bg-dark"
-                  }`}
-                />
-              ))}
-            </div>
-            <div className="flex gap-2 text-gray-950">
-              <div className="w-full h-12 dark:bg-dark bg-light flex items-center justify-center rounded-xl dark:text-light">
-                done
-              </div>
-              <div className="w-full h-12 bg-light dark:bg-dark flex items-center justify-center rounded-xl dark:text-light">
-                undo
-              </div>
-            </div>
-          </div>
-        </div>
-      ))}
+      <TaskBox taskList={activityList} />
       <div className="h-10 w-full"></div>
     </div>
   );
