bc18987ad2ec7d0d7129bf350f242325b11260d5
dieKrake <kkrazius@gmail.com>
2024-10-07 15:13:47 +0200

    added secret function

--- src/app/amplify-cognito-config.ts
+++ src/app/amplify-cognito-config.ts
@@ -1,11 +1,14 @@
 "use client";
 
 import { Amplify, type ResourcesConfig } from "aws-amplify";
+import { secret } from "@aws-amplify/backend";
 
 export const authConfig: ResourcesConfig["Auth"] = {
   Cognito: {
-    userPoolId: String(process.env.NEXT_PUBLIC_USER_POOL_ID),
-    userPoolClientId: String(process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID),
+    // userPoolId: String(process.env.NEXT_PUBLIC_USER_POOL_ID),
+    // userPoolClientId: String(process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID),
+    userPoolId: String(secret("NEXT_PUBLIC_USER_POOL_ID")),
+    userPoolClientId: String(secret("NEXT_PUBLIC_USER_POOL_CLIENT_ID")),
   },
 };
 
